775c4d0d36c26d1335d51c5189e6c06a67754dd0
HariPalempati <HariKrishna@Swathis-MacBook-Pro.local>
2015-06-27 22:00:51 -0700

    ..

---
+++ FriendsList/Friends.h
@@ -0,0 +1,13 @@
+//
+//  Friends.h
+//  FriendsList
+//
+//  Created by Hari Krishna Palempati on 27/06/15.
+//  Copyright (c) 2015 Hari Krishna Palempati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Friends : UIViewController
+
+@end
