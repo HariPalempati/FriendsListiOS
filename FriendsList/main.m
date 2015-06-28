5ad450499f7dfaa40401d5344fbd6bb5c02a772e
HariPalempati <HariKrishna@Swathis-MacBook-Pro.local>
2015-06-27 19:56:30 -0700

    Initial Commit

---
+++ FriendsList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FriendsList
+//
+//  Created by Hari Krishna Palempati on 27/06/15.
+//  Copyright (c) 2015 Hari Krishna Palempati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
